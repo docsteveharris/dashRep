5fd9c054b2abd31f37a8c0df249ea7cbf7ea2fff
Steve Harris <doc@steveharris.me>
2021-11-03 23:31:16 +0000

    prettify

--- app/app.py
+++ app/app.py
@@ -2,9 +2,12 @@ import dash
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__,
+                title='HYLODE',
+                update_title=None,
                 external_stylesheets=[
-                    dbc.themes.FLATLY,
+                    dbc.themes.SIMPLEX,
                     dbc.icons.FONT_AWESOME,
                 ],
-                suppress_callback_exceptions=True)
+                suppress_callback_exceptions=True
+                )
 server = app.server
